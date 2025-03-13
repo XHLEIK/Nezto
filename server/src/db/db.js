f14559427c5e317d18213af58f8645cbdf9807d9
hunter87ff <huntergaming702@gmail.com>
2025-03-13 15:30:10 +0530

    updated new models for more effective authorization and efficiency, removed necessary modules

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "@/constants.js"
+import {connect} from "mongoose";
+import {MONGO_URI} from "@/config.js"
 
 const connectDB = async () => {
     try {
-        const connectionIn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionIn = await connect(MONGO_URI)
         console.log("mongoDB connected",connectionIn.connection.host)
     } catch (error) {
         console.log("MongoDB Failed",error?.message)
