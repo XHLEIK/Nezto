9a0592bea71c76269ad30d70d117151c3f9cf9c8
hunter87ff <huntergaming702@gmail.com>
2025-03-21 10:04:49 +0530

    🚀Add new icons and update service cards with improved pricing and descriptions

--- client/src/component/block/NavBar.jsx
+++ client/src/component/block/NavBar.jsx
@@ -6,7 +6,9 @@ function NavBar() {
 
         <header>
             <div className="flex w-full max-w-[680px] justify-between items-center p-4 fixed top-0 bg-primary rounded-b-3xl">
-                <div onClick={()=>navigate("/")} className="text-2xl font-bold text-white cursor-pointer">NEZTO</div>
+                <div onClick={()=>navigate("/")} className="text-2xl font-bold text-white cursor-pointer">
+                    <img src="/img/logo/logo-header.png" className="w-[100px]  max-h-7" alt="NEZTO" />
+                </div>
                 <div className="w-10 h-10 bg-white bg-opacity-20 cursor-pointer rounded-full flex items-center justify-center">
                     <User onClick={()=>navigate("/profile")} className="text-primary"/>
                 </div>
--- client/src/component/ui/cards/ServiceCard.jsx
+++ client/src/component/ui/cards/ServiceCard.jsx
@@ -3,7 +3,7 @@
 
 function ServiceCard({icon, cardTitle,cardDescription, cardPrice, cardButtonText="Book Now"}) {
     return (
-        <div  className="bg-teal-100 rounded-lg p-4 flex flex-col">
+        <div  className="bg-teal-100 morfit rounded-lg p-4 flex flex-col">
             <div className="flex items-center gap-3 mb-2">
                 <div className="bg-primary p-2 rounded-lg">
                     {icon}
@@ -13,7 +13,7 @@ function ServiceCard({icon, cardTitle,cardDescription, cardPrice, cardButtonText
             <p className="text-teal-600 text-sm mb-2">{cardDescription}</p>
             <div className="flex justify-between items-center mt-auto">
                 <div className="font-bold text-teal-700 text-lg">{cardPrice}</div>
-                <button className="bg-primary text-white px-3 py-1 rounded-lg text-sm">{cardButtonText}</button>
+                <button className="bg-primary text-white border hover:text-primary hover:bg-white hover:border-primary px-3 py-1 rounded-lg text-sm cursor-pointer">{cardButtonText}</button>
             </div>
         </div>
     )
